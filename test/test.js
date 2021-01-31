d61dcc35c2d9f5711b01835d8cf8fc1c69fea8e4
Hancheng Zhou <z308114274@gmail.com>
2021-01-31 21:44:19 +0800

    test

--- test/test.js
+++ test/test.js
@@ -1,18 +1,15 @@
-// const chai = require('chai');
-// const expect = chai.expect
-// chai.use(require('chai-as-promised'))
-// const assert = require('assert');
-// describe('Array', function() {
-//   describe('#indexOf()', function() {
-//     it('should return -1 when the value is not present',  (done) => {
-//       // await sleep()
-//       assert.equal([1, 2, 3].indexOf(4), 1);
-//       done()
-//       // expect([1, 2, 3].indexOf(2)).to.be.eq(-1)
-//     });
-//   });
-// });
+const chai = require('chai');
+const expect = chai.expect
+chai.use(require('chai-as-promised'))
+const assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', async () => {
+      assert.equal([1, 2, 3].indexOf(4), 1);
+    });
+  });
+});
 
-// function sleep() {
-//   return new Promise((resolve) => setTimeout(() => resolve(1000), 1500))
-// }
+function sleep() {
+  return new Promise((resolve) => setTimeout(() => resolve(1000), 1500))
+}
