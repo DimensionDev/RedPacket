e5584c94dec6ade1e786d176fdd9a0cbd0f27a24
Andy Jiang <andy@mask.io>
2021-06-09 21:11:16 -0700

    Make smart contract upgradeable

--- README.md
+++ README.md
@@ -32,6 +32,14 @@ truffle debug [TX_ID]
 
 ## Deployed Contract Address
 
-| Contract | [Mainnet](https://etherscan.io/) | [Ropsten](https://ropsten.etherscan.io/) | [BSC](https://bscscan.com/) |[BSC-testnet](https://testnet.bscscan.com/) | [Matic](https://matic.network/) | [Matic-mumbai](https://explorer-mumbai.maticvigil.com/) |
-|---|---|---|---|---|---|---|
-| [HappyRedPacket](contracts/redpacket.sol) | N/A | [0x5723E9D2](https://ropsten.etherscan.io/address/0x5723E9D205bb4dc46c71Fdf68c08d10fF9Efa65D) | [0x6cc1b105](https://bscscan.com/address/0x6cc1b1058F9153358278C35E0b2D382f1585854B) | [0x8440b99B](https://testnet.bscscan.com/address/0x8440b99B1Df5D4B61957c8Ce0a199487Be3De270) | [0x71834a3F](https://explorer-mainnet.maticvigil.com/address/0x71834a3FDeA3E70F14a93ED85c6be70925D0CAd9) | [0x6DfC82B4](https://explorer-mumbai.maticvigil.com/address/0x6DfC82B48CFd38d4722366Cd6F444a341b8840f5) |
+| Chain   | Address                    |
+| ------- | -------------------------- |
+| Mainnet | [0x7323ec10][ito-0x7323ec10] |
+| Ropsten | [0x52a80151][ito-0x52a80151] |
+| BSC     | [0x3EB4C32b][ito-0x3EB4C32b] |
+| Matic   | [0xCCf18218][ito-0xCCf18218] |
+
+[ito-0x7323ec10]: https://etherscan.io/address/0x7323ec104a689480dEbE8Eb1404FB0f9D425D2ca
+[ito-0x52a80151]: https://ropsten.etherscan.io/address/0x52a80151dDF3E1AffE6537c5f56191dD2d97c46C
+[ito-0x3EB4C32b]: https://bscscan.com/address/0x3EB4C32bB45ca9b6160476a2e839190BD60FA623
+[ito-0xCCf18218]: https://polygonscan.com/address/0xCCf182182376730c2c23Edc815bBdc714e91741c
--- deploy/redpacket.ts
+++ deploy/redpacket.ts
@@ -1,5 +1,17 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
+import { ethers, upgrades } from 'hardhat'
+
+type MyMapLikeType = Record<string, string>
+const deployedContracts: MyMapLikeType = {
+  mainnet: '0x7323ec104a689480dEbE8Eb1404FB0f9D425D2ca',
+  ropsten: '0x52a80151dDF3E1AffE6537c5f56191dD2d97c46C',
+  rinkeby: '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  bsc_test: '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  bsc_mainnet: '0x3EB4C32bB45ca9b6160476a2e839190BD60FA623',
+  matic_mumbai_test: '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  matic_mainnet: '0xCCf182182376730c2c23Edc815bBdc714e91741c',
+}
 
 const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
@@ -10,6 +22,21 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
     args: [],
     log: true,
   })
+
+  const network: string = hre.hardhatArguments.network ? hre.hardhatArguments.network : 'ropsten'
+  const proxyAddress = deployedContracts[network]
+
+  if (false) {
+    // deploy, we normally do this only once
+    const HappyRedPacketImpl = await ethers.getContractFactory('HappyRedPacket')
+    const HappyRedPacketProxy = await upgrades.deployProxy(HappyRedPacketImpl, [])
+    await HappyRedPacketProxy.deployed()
+    console.log('HappyRedPacketProxy: ' + HappyRedPacketProxy.address)
+  } else {
+    // upgrade contract
+    const HappyRedPacketImpl = await ethers.getContractFactory('HappyRedPacket')
+    await upgrades.upgradeProxy(proxyAddress, HappyRedPacketImpl)
+  }
 }
 
 func.tags = ['HappyRedPacket']
