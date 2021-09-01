346f848883f8e6653010befea429abecfb21941f
Andy Jiang <andy@mask.io>
2021-09-01 04:39:27 -0700

    Add standard ERC-20 token, same as test_token.sol

--- contracts/test_token.sol
+++ contracts/test_token.sol
@@ -5,7 +5,7 @@ pragma solidity >0.4.22;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor(uint initialSupply) ERC20("TestToken", "TEST") public{
+    constructor(uint initialSupply) ERC20("TestToken", "TEST") {
         _mint(msg.sender, initialSupply);
     }
 }
