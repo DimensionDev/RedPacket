c3d6405fbfaa40deb34bd635867905475ed8c0fc
Andy Jiang <83930369+andy-at-mask@users.noreply.github.com>
2021-09-16 00:39:07 +1200

    Add deployed address (#29)

--- deploy/erc721_support.ts
+++ deploy/erc721_support.ts
@@ -4,22 +4,12 @@ import { ethers, upgrades } from 'hardhat'
 
 type MyMapLikeType = Record<string, string>
 const deployedContracts: MyMapLikeType = {
-  // mainnet: '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  // ropsten: '0xcA6589d246355EaFBE424De335fd937Dcbd619A2',
-  // rinkeby: '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  // bsc_test: '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  // bsc_mainnet: '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  // matic_mumbai_test: '0xF8a45d2F1a4d13Cd82ffeDd8c7747a15D0309571',
-  // matic_mainnet: '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  // arbitrum: '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  // arbitrum_rinkeby: '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  mainnet: '0xaBBe1101FD8fa5847c452A6D70C8655532B03C33',
-  ropsten: '0x0722507c3b776A6B205946592016e358B0D34c3F',
-  bsc_mainnet: '0x0ca42C178e14c618c81B8438043F27d9D38145f6',
-  matic_mainnet: '0x93e0b87A0aD0C991dc1B5176ddCD850c9a78aabb',
-  arbitrum: '0x83D6b366f21e413f214EB077D5378478e71a5eD2',
-  arbitrum_rinkeby: '0x4A77E797031257db72F7D2C3Ec08a4FAc5c8CfE9',
-  xdai: '0x54a0A221C25Fc0a347EC929cFC5db0be17fA2a2B',
+  mainnet: '0x8d285739523FC2Ac8eC9c9C229ee863C8C9bF8C8',
+  ropsten: '0x8fF42e93C19E44763FD1cD07b9E04d13bA07AD3f',
+  bsc_mainnet: '0xf8968e1Fcf1440Be5Cec7Bb495bcee79753d5E06',
+  matic_mainnet: '0xf6Dc042717EF4C097348bE00f4BaE688dcaDD4eA',
+  arbitrum: '0x561c5f3a19871ecb1273D6D8eCc276BeEDa5c8b4',
+  xdai: '0x561c5f3a19871ecb1273D6D8eCc276BeEDa5c8b4',
 }
 
 const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
@@ -29,7 +19,7 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
   const network: string = hre.hardhatArguments.network ? hre.hardhatArguments.network : 'ropsten'
   const proxyAddress = deployedContracts[network]
 
-  if (true) {
+  if (false) {
     // deploy, we normally do this only once
     const HappyRedPacketImpl_erc721 = await ethers.getContractFactory('HappyRedPacket_ERC721')
     const HappyRedPacketProxy_erc721 = await upgrades.deployProxy(HappyRedPacketImpl_erc721, [])
