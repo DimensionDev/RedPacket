73e3bd210e7f50c1ce8bcf3d265c828fc9ed3822
Andy Jiang <andy@mask.io>
2021-12-16 17:14:00 +1300

    Add Avax, Fantom, Celo, Optimism, Aurora Fuse, Boba and Moonriver

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,19 +7,22 @@ import 'hardhat-deploy-ethers'
 import "@nomiclabs/hardhat-waffle"
 import "@nomiclabs/hardhat-ethers"
 import "solidity-coverage"
+import "hardhat-gas-reporter"
 import '@openzeppelin/hardhat-upgrades'
 import "@nomiclabs/hardhat-truffle5"
-import "hardhat-gas-reporter"
+import "@nomiclabs/hardhat-etherscan";
 
 const {
     HardhatNetworkConfig,
     HardhatSolidityConfig,
     HardhatGasReporterConfig,
+    EtherscanConfig,
 } = require('./SmartContractProjectConfig/config.js');
 
 const networks = HardhatNetworkConfig;
 const solidity = HardhatSolidityConfig;
 const gasReporter = HardhatGasReporterConfig;
+const etherscan = EtherscanConfig;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -35,5 +38,6 @@ module.exports = {
             default: 0,
         },
     },
+    etherscan,
     gasReporter,
 };
